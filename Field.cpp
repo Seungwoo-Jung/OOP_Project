8d30894c8e5214a7367aad401b1cd53f652aa062
mlardelli <markus@LAPTOP-D1HVNL1G>
2023-10-11 16:12:12 +1030

    Added code to header and cpp

---
+++ Field.cpp
@@ -0,0 +1,15 @@
+#include "Field.h"
+
+Field::Field(){
+    soilState = "default";
+    fieldSizeM = 1;
+    fieldSizeN = 1;
+    fieldMatrix = new Plant*[fieldSizeM][fieldSizeN];
+}
+
+Field::Field(int size_row, int size_column){
+    soilState = "default";
+    fieldSizeM = size_row;
+    fieldSizeN = size_column;
+    fieldMatrix = new Plant*[fieldSizeM][fieldSizeN];
+}
