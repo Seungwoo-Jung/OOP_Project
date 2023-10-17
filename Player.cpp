0e828217a3513aa76f0b8d356891057ad3d49bea
HabitualRapidity24 <a1868900@adelaide.edu.au>
2023-10-17 15:33:29 +1030

    hi

--- Player.cpp
+++ Player.cpp
@@ -6,16 +6,12 @@
 
 using namespace std;
 
-<<<<<<< HEAD
 Player::Player() {
   cout << "Enter numbers to create your userID: " << endl;
   cin >> userID;
   timepassed = 0;
   validInput = false;
 }
-=======
-Player::Player(int initialMoney) : Currency(initialMoney) {};
->>>>>>> 25f174546402ee47766097f73d4208065f575038
 
 char Player::Navigate() {
   char input = '0';
@@ -38,10 +34,4 @@ void Player::saveData() {
   saveFile.close();
 }
 
-<<<<<<< HEAD
 Player::~Player() {}
-=======
-Player::~Player() {
-    
-}
->>>>>>> 25f174546402ee47766097f73d4208065f575038
