a6301994f7b05c9c93a449751573cf9ca824eea0
HabitualRapidity24 <a1868900@adelaide.edu.au>
2023-10-17 15:25:50 +1030

    merging branches

--- Player.h
+++ Player.h
@@ -1,20 +1,27 @@
 #ifndef PLAYER_H
 #define PLAYER_H
 #include <iostream>
-#include <string>
 #include <map>
+#include <string>
 
 using namespace std;
 
 class Player {
-    private:
-        int userID;
-        int Currency;
-        map<string, int> inventory;
-    public:
-        Player();
-        void loadData();
-        void saveData();
-        ~Player();
+ private:
+  int userID;
+  int timepassed;
+  bool validInput;
+  map<string, int> inventory;
+
+ public:
+  Player();
+  void loadData();
+  void saveData();
+  void passTime();
+  char Navigate();
+  void changeFarm();
+  void exchangeItem();
+
+  ~Player();
 };
 #endif
